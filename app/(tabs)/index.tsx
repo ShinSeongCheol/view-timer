a585e1b0ad403bf1e3115889ce59d9b1e2140ee5
shinseongcheol <cher3725@gmail.com>
2025-11-18 11:35:11 +0900

    :lipstick: feat 'tab'
    - 하단 탭 추가

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import '@/styles/global.css'
+
+export default function Index() {
+    return (
+        <View className={'flex-1 justify-center items-center'}>
+            <Text>HI</Text>
+        </View>
+    );
+}
